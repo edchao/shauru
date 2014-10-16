5a4575863c926147e92fd1e5084aee22a6260d28
Ed Chao <edchao@eds-macbook-pro.hsd1.ca.comcast.net>
2014-10-15 22:37:01 -0700

    badabing

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,18 +14,46 @@ module.exports = function(grunt) {
 
     shell : {
         jekyllBuild : {
-            command : 'jekyll serve'
+            command : 'jekyll build'
         },
+    },
+
+    watch : {
+      files : [ '_layouts/*.html',
+                '_posts/*.markdown',
+                'sass/*.scss',
+                'sass/*/*.scss',
+                '_config.yml',
+                'index.html'],
+      tasks : ['sass','shell' ],
+      options : {
+          spawn : false,
+          interrupt : true,
+          atBegin : true,
+          livereload : true
+      }
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          base: '_site'
+        }
+      }
     }
 
 
   });
 
   // Load the plugins
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
 
   // Default task(s).
-  grunt.registerTask('default', ['sass', 'shell']);
+  grunt.registerTask('default', ['connect', 'watch']);
 
 };
